a40bc4f4559b87f65d3751048ab1105e28553829
dorsch95 <123198704+dorsch95@users.noreply.github.com>
2026-08-28 19:16:10 +0200

    Update config_flow.py

--- custom_components/eta_webservices/config_flow.py
+++ custom_components/eta_webservices/config_flow.py
@@ -5,27 +5,26 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
 
-from .const import DOMAIN, DEFAULT_PORT
+from .const import DOMAIN, DEFAULT_PORT, SCHEMAS
 
 class ETAConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Verwaltet den Setup-Flow für ETA Webservices."""
+    """Verwaltet den Setup-Flow für ETA Webservices mit Schemaauswahl."""
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Erster Schritt bei der manuellen Einrichtung."""
         errors = {}
 
         if user_input is not None:
             host = user_input["host"]
             port = user_input["port"]
             
-            # Teste die Verbindung, bevor die Integration gespeichert wird
             session = async_get_clientsession(self.hass)
             try:
+                # Teste ob die Heizung unter der IP erreichbar ist
                 async with session.get(f"http://{host}:{port}/user/menu", timeout=5) as response:
                     if response.status == 200:
                         return self.async_create_entry(
-                            title=f"ETA Heizung ({host})", 
+                            title=f"ETA Heizung ({user_input['schema']})", 
                             data=user_input
                         )
                     else:
@@ -33,10 +32,11 @@ class ETAConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             except Exception:
                 errors["base"] = "cannot_connect"
 
-        # Definition des Eingabeformulars
+        # Definition des Eingabeformulars inklusive Dropdown für Schemen
         data_schema = vol.Schema({
             vol.Required("host"): cv.string,
             vol.Required("port", default=DEFAULT_PORT): cv.port,
+            vol.Required("schema", default="Kessel + Puffer"): vol.In(list(SCHEMAS.keys())),
         })
 
         return self.async_show_form(
